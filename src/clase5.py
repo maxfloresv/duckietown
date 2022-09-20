f67284d0c4b91d06211745e88511803ce7db75b2
Máximo Flores Valenzuela <maximo.flores@ug.uchile.cl>
2022-09-20 19:50:05 -0300

    Update clase5.py

--- src/clase5.py
+++ src/clase5.py
@@ -25,21 +25,21 @@ class Template(object):
     
 		mask = cv2.inRange(image_out, lower_limit, upper_limit)
 		
-    kernel = np.ones((5, 5), np.uint8)
-    img_out = cv2.erode(mask, kernel, iterations=3)
-    img_out = cv2.dilate(img_out, kernel, iterations=3)
+    		kernel = np.ones((5, 5), np.uint8)
+    		img_out = cv2.erode(mask, kernel, iterations=3)
+    		img_out = cv2.dilate(img_out, kernel, iterations=3)
     
-    img_masked = cv2.bitwise_and(image, image, mask=mask)
+    		img_masked = cv2.bitwise_and(image, image, mask=mask)
     
-    _, contours, hierarchy = cv2.findContours(img_out, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    min_area = 5
+    		_, contours, hierarchy = cv2.findContours(img_out, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    		min_area = 5
     
-    for val in contours:
-      x, y, w, h = cv2.boundingRect(val)
-      if w*h > min_area:
-        cv2.rectangle(image, (x+w, y+h), (x, y), (0, 0, 0), 2)
+    		for val in contours:
+      			x, y, w, h = cv2.boundingRect(val)
+      			if w*h > min_area:
+        			cv2.rectangle(image, (x+w, y+h), (x, y), (0, 0, 0), 2)
         
-    msg = bridge.cv2_to_imgmsg(image, "bgr8")
+    		msg = bridge.cv2_to_imgmsg(image, "bgr8")
 		self.pub.publish(msg)
 
 
